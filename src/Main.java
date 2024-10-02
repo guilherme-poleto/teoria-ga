d85e555df0696423535f2f00674e97a108934c6b
guilherme-poleto <guipoleto@edu.unisinos.br>
2024-10-01 23:13:19 -0300

    add fibonacci implementation

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,14 @@ public class Main {
 
     public static void main(String[] args) {
 
-        EliasGamma eg = new EliasGamma();
         Fibonacci fn = new Fibonacci();
+        String message = "hello world!";
+        String encoded = fn.encode(message);
+        System.out.println(encoded);
+        System.out.println(fn.decode(encoded));
+
         Ui ui = new Ui();
         ui.usuario_msg();
         ui.escolhe_metodo_compressao();
-        
-
     }
 }
--- src/algoritmos/Fibonacci.java
+++ src/algoritmos/Fibonacci.java
@@ -4,17 +4,82 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class Fibonacci {
+    private final List<Integer> decoderFibonacciList;
 
-    public void encode() {
-        int number = 40;
+    public Fibonacci() {
+        this.decoderFibonacciList = buildFibonacciList(127);
+    }
+
+    public String encode(String message) {
+        StringBuilder result = new StringBuilder();
+
+        for (char c : message.toCharArray()) {
+            String codeWord = encodeNumber(c);
+            result.append(codeWord).append(" ");
+        }
+
+        return result.toString();
+    }
+
+    public String decode(String encodedMessage) {
+        StringBuilder result = new StringBuilder();
+        String[] encodedChars = encodedMessage.split(" ");
+
+        for (String encodedChar : encodedChars) {
+            int decodedAscii = decodeBinary(encodedChar);
+            result.append((char) decodedAscii);
+        }
+
+        return result.toString();
+    }
+
+    private String encodeNumber(int number) {
+        List<Integer> fibonacci = buildFibonacciList(number);
+
+        StringBuilder result = new StringBuilder();
+        int reducedNumber = number;
+
+        for (int i = fibonacci.size() - 1; i >= 0; i--) {
+            int value = fibonacci.get(i);
+            if (value <= reducedNumber) {
+                reducedNumber -= value;
+                result.append("1");
+            } else {
+                if (!result.isEmpty()) {
+                    result.append("0");
+                }
+            }
+        }
+        return result.toString();
+    }
+
+    private int decodeBinary(String binary) {
+        int num = 0;
+        int length = binary.length();
+
+        for (int i = 0; i < length; i++) {
+            if (binary.charAt(i) == '1') {
+                num += decoderFibonacciList.get(length - i - 1);
+            }
+        }
+
+        return num;
+    }
+
+    private static List<Integer> buildFibonacciList(int number) {
         List<Integer> fibonacci = new ArrayList<>();
         fibonacci.add(1);
         fibonacci.add(2);
-        int currentValue = 2;
+        int counter = 2;
+        int nextValue = 3;
 
-        while (currentValue <= number) {
-            fibonacci.add(1);
+        while (nextValue <= number) {
+            nextValue = fibonacci.get(counter - 1) + fibonacci.get(counter - 2);
+            if (nextValue <= number) {
+                fibonacci.add(nextValue);
+            }
+            counter++;
         }
-
+        return fibonacci;
     }
 }
